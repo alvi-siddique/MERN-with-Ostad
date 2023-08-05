7e8fc72c205bb3aa16437a62f5c252a70d27208f
alvi-siddique <alvisiddique567@gmail.com>
2023-08-05 22:34:34 +0600

    Ostad Assignments

---
+++ Assingment-17/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDoView from "./redux/features/todo/toDoView";
+
+const App = () => {
+	return (
+		<div>
+			<ToDoView />
+		</div>
+	);
+};
+
+export default App;
